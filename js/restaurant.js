109908c73e2b28d7433de31f2fe2e0cf81f8d307
Thierry <thierry.raimond@wanadoo.fr>
2017-04-25 18:58:44 +0200

    etape2 ajouter un avis

--- js/restaurant.js
+++ js/restaurant.js
@@ -32,18 +32,12 @@ var propStreetView = {
 	size : "300x150"	
 };
 
-
 genererListeRestaurants();
 
 // Genere la liste des restaurants
 function genererListeRestaurants() {
 	for (i in restaurants) {
-		var totalEtoile = 0;
-		for (j in restaurants[i].ratings) {				
-			totalEtoile += restaurants[i].ratings[j].stars;
-		}
-		var moyenneAvis = totalEtoile/restaurants[i].ratings.length;
-		console.log("moyenne Avis = " + moyenneAvis);
+		var moyenneAvis = calculerMoyenneAvis(i);
 		var urlStreetView = 'https://maps.googleapis.com/maps/api/streetview?'+
 			'size=' + propStreetView.size +
 			'&location=' + restaurants[i].lat + ',' + restaurants[i].long +
@@ -59,11 +53,35 @@ function genererListeRestaurants() {
 						'<p class="list-group-item-text">'+
 							restaurants[i].address +'<br/>'+
 							restaurants[i].lat + ', ' + restaurants[i].long +'<br/>'+
-							'<span id="moyenneAvis'+i+'" style="color:orange; font-size:15px"><strong>'+
+							'<strong><span id="moyenneAvis'+i+'" style="color:orange; font-size:15px">'+
 								moyenneAvis +
-							'</strong></span> - ' +
-							restaurants[i].ratings.length + ' avis'+					
+							'</span></strong> - ' +
+							'<span id="nbAvis'+i+'">' + restaurants[i].ratings.length + ' avis</span>'+					
 						'</p>'+
+					'</div>'+
+					'<div class="col-xs-12" style="padding-top:10px;">'+
+						'<div class="row">'+
+							'<div class="col-xs-2 form-group">'+
+								'<button class="btnAjouterAvis btn btn-primary" id="btnAjouterAvis'+i+'" title="Ajouter un nouvel avis">'+
+									'<span class="glyphicon glyphicon-plus"></span>'+
+								'</button>'+
+							'</div>'+
+							'<div class="col-xs-10 form-group" id="ajouterNouveauAvis'+i+'" style="display:none;">'+
+								'<div>'+
+									'<i class="fa fa-star star" id="star1_'+i+'" ></i>'+
+									'<i class="fa fa-star star" id="star2_'+i+'" ></i>'+
+									'<i class="fa fa-star star" id="star3_'+i+'" ></i>'+
+									'<i class="fa fa-star star" id="star4_'+i+'" ></i>'+
+									'<i class="fa fa-star star" id="star5_'+i+'" ></i>'+
+									'<span id="note'+i+'" style="display:none;">0</span>'+
+								'</div>'+
+								'<div class="input-group">'+
+									'<input class="form-control" id="nouveauAvis'+i+'" name="nouveauAvis" placeholder="Saisissez un nouvel avis" value="" type="text" required>'+
+									'<span class="input-group-btn"><button type="submit" class="btn btn-success validerNouveauAvis" id="btnValiderNouveauAvis'+i+'"><span class="glyphicon glyphicon-ok"></span></button></span>'+
+								'</div>'+
+								'<div class="alert alert-danger" style="display:none;" id="alert'+i+'" role="alert">erreur</div>'+
+							'</div>'+
+						'</div>'+		
 					'</div>'+
 					'<div class="col-xs-12 collapse" id="collapse'+i+'"></div>'+
 				'</div>'+
@@ -78,6 +96,88 @@ function genererListeRestaurants() {
 	}
 };
 
+
+$('.btnAjouterAvis').on('click', function(){
+	var id = $(this).attr('id').replace('btnAjouterAvis','');
+	$('#ajouterNouveauAvis'+id).toggle(500);
+});
+
+$('.star').on('mouseenter', function(){
+	var star = idEtoile($(this).attr('id'));
+	for (var n=1 ; n <= star.numStar; n++){
+		$('#star'+n+'_'+star.i).css('color', 'orange');
+	}	
+});
+$('.star').on('mouseleave', function(){
+		$('.star').css('color', 'grey');
+});
+
+$('.star').on('click', function(){
+	var star = idEtoile($(this).attr('id'));
+	// remise à zero de toutes les anciennes étoiles cliqués (notées)
+	for (var n=1 ; n <= 5; n++){
+		$('#star'+n+'_'+star.i).removeClass('note').addClass('star');
+	}	
+	// nouvelle note
+	for (var n=1 ; n <= star.numStar; n++){
+		$('#star'+n+'_'+star.i).removeClass('star').addClass('note');
+	}
+	// une note est attribuée
+	$('#note'+star.i).text(star.numStar);
+});
+
+function idEtoile (that){
+	for (i in restaurants) {
+		for (var numStar=1 ; numStar<=5; numStar++) {
+			if(that == 'star'+numStar+'_'+i) {
+				var star = {i: i, numStar: numStar};
+				return star;
+				break;
+			}		
+		}
+	}
+};
+
+function calculerMoyenneAvis(index) {
+	var totalEtoile = 0;
+	for (j in restaurants[index].ratings) {				
+		totalEtoile += restaurants[index].ratings[j].stars;
+	}
+	return (totalEtoile/restaurants[index].ratings.length).toFixed(1); // arrondit à 1 chiffre après la virgule
+};
+
+$('.validerNouveauAvis').on('click', function(){
+	var id = $(this).attr('id').replace('btnValiderNouveauAvis','');
+	var n = restaurants[id].ratings.length;
+	var note = parseInt($('#note'+id).text());
+	// contrôle qu'une note a été attribuée et qu'un commentaire est renseigné
+	if($('#nouveauAvis'+id).val() == '' || note == 0) {
+		$('#alert'+id).removeClass().addClass('alert alert-danger')
+			.text('le champ commentaire est vide ou aucune note est attribuée')
+			.show(500).delay(3000).hide(500);
+	} else {
+		restaurants[id].ratings[n] = { "stars":note, "comment": $('#nouveauAvis'+id).val() };
+		$('#collapse'+id).append(
+				'<span style="color:orange; font-size:14px">' + restaurants[id].ratings[n].stars + '/5 '+'</span>'+
+				'<span style="font-size:12px">\"'+ restaurants[id].ratings[n].comment+'\"</span><br/>'
+		);
+		//TODO mettre à jour la moyenne des avis et le nombre d'avis
+		$('#moyenneAvis'+id).text(calculerMoyenneAvis(id));
+		$('#nbAvis'+id).text(restaurants[id].ratings.length + " avis");
+		
+		$('#alert'+id).removeClass().addClass('alert alert-success')
+			.text('nouvel avis enregistré avec succès')
+			.show(500).delay(3000).hide(500);
+		
+		//TODO réinitialiser les valeurs par défaut du formulaire et le cacher
+		//$('#ajouterNouveauAvis'+id).toggle(500);
+		$('#nouveauAvis'+id).val('');
+		// remise à zero de toutes les anciennes étoiles cliqués (notées)
+		for (var j=1 ; j <= 5; j++){
+			$('#star'+j+'_'+id).removeClass('note').addClass('star').css('color', 'grey');
+		}	
+	}
+});
 
 
 
